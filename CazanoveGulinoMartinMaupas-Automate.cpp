ba732edd745cb6b00d30621b2656a08174e10e98
MOPZ <paul.maupas@gmail.com>
2014-05-18 17:00:18 +0200

    V1.2

--- CazanoveGulinoMartinMaupas-Automate.cpp
+++ CazanoveGulinoMartinMaupas-Automate.cpp
@@ -9,15 +9,22 @@ bool operator==(const State& s1, const State& s2)
     return false;
 }
 
-ostream& operator<<(ostream &os, const State& state)
+ostream& operator<<(ostream &os, const State& states)
 {
-    for (State::iterator it = state.begin(); it != state.end(); it++)
+    for (State::iterator it = states.begin(); it != states.end(); it++)
     {
-        if (it != state.begin())
+        if (it != states.begin())
             os << ",";
         os << *it;
     }
     return os;
+}
+
+bool operator==(const t_transition& t1, const t_transition& t2)
+{
+    if (t1.b == t2.b && t1.e == t2.e && t1.c == t2.c)
+        return true;
+    return false;
 }
 
 Automate::Automate() : _language(), _transitions(), _input(), _output()
@@ -56,7 +63,7 @@ void Automate::add_to_language(char c)
     _language.push_back(c);
 }
 
-void Automate::add_state(const State &states, bool in, bool out)
+void Automate::add_state(State states, bool in, bool out)
 {
     Transitions old = _transitions[states];
     _transitions[states] = old;
@@ -81,9 +88,9 @@ void Automate::remove_state(state_type state)
     remove_state(s);
 }
 
-void Automate::add_input(const State &states)
+void Automate::add_input(State &states)
 {
-    _input.push_back(states);
+    _input.insert(states);
 }
 
 void Automate::add_input(state_type state)
@@ -93,9 +100,9 @@ void Automate::add_input(state_type state)
     add_input(s);
 }
 
-void Automate::add_output(const State &states)
+void Automate::add_output(State &states)
 {
-    _output.push_back(states);
+    _output.insert(states);
 }
 
 void Automate::add_output(state_type state)
@@ -105,11 +112,24 @@ void Automate::add_output(state_type state)
     add_output(s);
 }
 
-void Automate::add_transition(const State &b, const State &e, char c)
+void Automate::add_transition(State b, State e, char c)
 {
     t_transition t = {b, e, c};
+    for (Transitions::iterator it = _transitions[b].begin(); it != _transitions[b].end(); it++)
+    {
+        if (*it == t)
+            return;
+    }
     _transitions[b].push_back(t);
     add_state(e);
+}
+
+void Automate::remove_transition(State &b, State &e, char c)
+{
+    t_transition t = {b, e, c};
+    Transitions::iterator it = find(_transitions[b].begin(), _transitions[b].end(), t);
+
+    _transitions[b].erase(it);
 }
 
 void Automate::add_transition(state_type b, state_type e, char c)
@@ -132,15 +152,68 @@ void Automate::display_transitions() const
         // it->first = State
         // it->second = Transitions
         gotoxy(x, y);
-        display_state(it->first);
+        cout << it->first << ":";
         // it2 = t_transition
         for (Transitions::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++)
         {
-            cout << " " << it2->c << it2->e;
+            if (it2 != it->second.begin())
+                cout << ",";
+            cout << " " << it2->c << " " << it2->e;
         }
         x = xi;
         y++;
     }
+}
+
+bool Automate::is_standard() const
+{
+    // Check nombre d'états initiaux
+    if (_input.size() > 1)
+    {
+        cout << "Non-standard: possede plus d'un etat initial." << endl;
+        return false;
+    }
+    // Check transitions vers état inital
+    for (map<State, Transitions>::const_iterator it = _transitions.begin(); it != _transitions.end(); it++)
+    {
+        for (Transitions::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++)
+        {
+            if (it2->e == (*_input.begin()))
+            {
+                cout << "Non-standard: " << it2->b << it2->c << it2->e << " mene vers un etat initial." << endl;
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Automate::make_it_standard()
+{
+    if (!this->is_standard())
+    {
+        State state;
+        state.insert("i");
+        this->add_state(state);
+
+        Transitions to_add;
+        for (map<State, Transitions>::iterator it = _transitions.begin(); it != _transitions.end(); it++)
+        {
+            if (_input.find(it->first) != _input.end())
+            {
+                for (Transitions::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++)
+                {
+                    to_add.push_back(*it2);
+                }
+            }
+        }
+        for (Transitions::iterator it = to_add.begin(); it != to_add.end(); it++)
+        {
+            this->add_transition(state, it->e, it->c);
+            this->remove_transition(it->b, it->e, it->c);
+        }
+    }
+    cout << "L'automate est maintenant standard." << endl;
 }
 
 
--- CazanoveGulinoMartinMaupas-Automate.h
+++ CazanoveGulinoMartinMaupas-Automate.h
@@ -23,13 +23,15 @@ struct t_transition
     char c;
 };
 
-typedef std::list<t_transition> Transitions;
+bool operator==(const t_transition&, const t_transition&);
+
+typedef std::vector<t_transition> Transitions;
 
 class Automate
 {
     std::list<char> _language;
     std::map<State, Transitions> _transitions;
-    std::list<State> _input, _output;
+    std::set<State> _input, _output;
 
     public:
         Automate();
@@ -42,32 +44,29 @@ class Automate
         void add_to_language(char);
 
         /* States */
-        void add_state(const State &states, bool in = false, bool out = false);
+        void add_state(State states, bool in = false, bool out = false);
         void add_state(state_type, bool in = false, bool out = false);
         void remove_state(const State &);
         void remove_state(state_type state);
 
         /* Initial and terminal */
-        void add_input(const State &states);
+        void add_input(State &states);
         void add_input(state_type);
-        void add_output(const State &states);
+        void add_output(State &states);
         void add_output(state_type);
 
         /* Transitions */
-        void add_transition(const State &b, const State &e, char c = '\0');
+        void add_transition(State b, State e, char c = '\0');
         void add_transition(state_type b, state_type e, char c);
+        void remove_transition(State &b, State &e, char c);
+        void remove_transition(state_type &b, state_type &e, char c);
 
         /* Display */
         void display_transitions() const;
-        static void display_state(const State &state)
-        {
-            for (State::iterator it = state.begin(); it != state.end(); it++)
-            {
-                if (it != state.begin())
-                    std::cout << ",";
-                std::cout << *it;
-            }
-        }
+
+        /* Traitement */
+        bool is_standard() const;
+        void make_it_standard();
 
     protected:
     private:
